e5dcbe23047609dae45c80033a54f2e94f64dbe3
MArtinOuss <oussamalamrabtii@gmail.com>
2022-11-17 12:29:15 +0100

    styling sign in button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,18 +8,19 @@ export const Navbar = () => {
         <div className='px-2 w-full h-full flex justify-between'>
             <div className='flex items-center'>
                 <h1 className='text-white text-xl font-bold text-center mr-2 '> IrriManager. </h1>
-                <ul className='hidden md:flex'>
+                <ul className='hidden pl-8 md:flex'>
                     <li>Home</li>
                     <li>About</li>
                 </ul>
             </div>
-            <div className='hidden md:flex pr-4'>
-                <button> Log In </button>
+            <div className='hidden md:flex items-center justify-end pr-8'>
+                <button className='h-12  p-1 px-4 text-slate-900 font-bold border border-white  bg-slate-300  hover:bg-transparent hover:text-white   rounded-md'> 
+                Sign In </button>
 
             </div>
           
 
-            <HiMenu className='my-2 w-16 h-7 fill-white'/>
+            <HiMenu className='my-2 w-16 h-7 fill-white hover:cursor-pointer'/>
 
         </div>
    
